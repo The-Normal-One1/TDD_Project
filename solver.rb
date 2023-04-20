a72b26e8b118c839a40a664684a0dfa762fd939f
Ermi <manermidem@gmail.com>
2023-04-20 20:30:04 +0300

    Fix linters

--- solver.rb
+++ solver.rb
@@ -2,13 +2,11 @@ class Solver
   def factorial(num)
     return 1 if num.zero?
 
-
     num * factorial(num - 1)
   end
 
   def reverse(word)
     return word if word.length <= 1
-
 
     reverse(word[1..]) + word[0]
   end
