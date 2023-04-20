d344809f7c9e1a7758129087660e95738b41fc55
Ermi <manermidem@gmail.com>
2023-04-20 19:57:11 +0300

    Add a test for reverse method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -14,4 +14,17 @@ end
         result = @solver.factorial(10)
         expect(result).to eq(3628800)
     end
+
+    describe "#reverse" do
+    
+        it "should return 'olleh'" do
+            result = @solver.reverse("hello")
+            expect(result).to eq("olleh")
+        end
+
+        it "should return 'dlrow'" do
+            result = @solver.reverse("world")
+            expect(result).to eq("dlrow")
+        end
+    end
 end
