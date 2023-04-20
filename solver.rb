204630ae0c87cc5c73a821b11edb0b69f376662f
huzaifakhan0308 <huzaifa031252khan@gmail.com>
2023-04-21 00:47:20 +0500

    add a method for fizzbuzz

--- solver.rb
+++ solver.rb
@@ -10,4 +10,16 @@ class Solver
 
     reverse(word[1..]) + word[0]
   end
+
+  def fizzbuzz(n)
+        if n % 15 == 0
+            return "fizzbuzz"
+        elsif n % 5 == 0
+            return "buzz"
+        elsif n % 3 == 0
+            return "fizz"
+        else
+            return n.to_s
+        end
+    end
 end
