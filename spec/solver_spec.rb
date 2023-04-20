4d6656b0d021470b371589fd259d36cfa5d703ac
Ermi <manermidem@gmail.com>
2023-04-20 19:53:23 +0300

    Modify the solver_spec.rb file

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,15 +1,16 @@
 require_relative "../solver.rb"
 
 describe Solver do
+before(:each) do
+    @solver = Solver.new
+end
 
     it "should return 120" do
-        @solver = Solver.new
         result = @solver.factorial(5)
         expect(result).to eq(120)
     end
 
     it "should return 3628800" do
-        @solver = Solver.new
         result = @solver.factorial(10)
         expect(result).to eq(3628800)
     end
